a6f1b3189ac50cd2d597a7e081354328a7ac3b71
Rafath-Auvee <rbza98@outlook.com>
2023-11-16 15:46:24 +0600

    setup

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 
 const Success = () => {
   const searchParams = useSearchParams()
-  const trx_id = searchParams.get('trx_id') // Extracting trx_id from the query parameters
+  const trxID = searchParams.get('trx_id') // Extracting trx_id from the query parameters
 
   return (
     <div>
       <h1>Success</h1>
-      {trx_id && <p>Transaction ID: {trx_id}</p>}
+      {trxID && <p>Transaction ID: {trxID}</p>}
     </div>
   );
 };
